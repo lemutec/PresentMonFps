1e9ae06c63563bc258907dde22efa61f061bf860
ema <mccoy39082@163.com>
2026-04-17 00:31:10 +0800

    feat: add PowerShell NuGet pack script

---
+++ build/nuget_pack.ps1
@@ -0,0 +1,20 @@
+﻿Set-Location $PSScriptRoot
+
+Write-Host @"
+███╗   ██╗██╗   ██╗ ██████╗ ███████╗████████╗
+████╗  ██║██║   ██║██╔════╝ ██╔════╝╚══██╔══╝
+██╔██╗ ██║██║   ██║██║  ███╗█████╗     ██║   
+██║╚██╗██║██║   ██║██║   ██║██╔══╝     ██║   
+██║ ╚████║╚██████╔╝╚██████╔╝███████╗   ██║   
+╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   
+"@
+
+Push-Location ../src/
+Write-Host "Processing PresentMonFps..."
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release -o ../build/
+Pop-Location
+
+Write-Host "`nPress any key to exit..."
+[void][System.Console]::ReadKey($true)
--- nuget_pack.cmd
+++
@@ -1,6 +0,0 @@
-@REM rd /s /q C:\Users\{UserName}\.nuget\packages\PresentMonFps
-@REM dotnet nuget locals all --clear
-cd src
-dotnet restore
-dotnet pack PresentMonFps.csproj -c Release -o ../
-@pause
