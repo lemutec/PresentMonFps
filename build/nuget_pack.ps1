a5232bd304ad9b083697dd411a278474c957635b
ema <mccoy39082@163.com>
2026-04-17 01:04:21 +0800

    fix: build script errors

--- build/nuget_pack.ps1
+++ build/nuget_pack.ps1
@@ -12,8 +12,8 @@ Write-Host @"
 Push-Location ../src/
 Write-Host "Processing PresentMonFps..."
 dotnet restore
-dotnet build -c Release
-dotnet pack -c Release -o ../build/
+dotnet build -c Release PresentMonFps.csproj
+dotnet pack -c Release -o ../build/ PresentMonFps.csproj
 Pop-Location
 
 Write-Host "`nPress any key to exit..."
